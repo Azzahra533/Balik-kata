54fc9fee24ff390963f8f947836a0e31a6bdae5f
26218 <26218@26218-PC>
2021-07-11 21:10:48 +0700

    Balik kata

---
+++ src/Balikkata.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 26218
+ */
+public class Balikkata {
+
+    public static void main(String[] args) {
+    StringBuffer kataBalik;
+    kataBalik = new StringBuffer("Selamat datang di dunia pemrograman");
+    System.out.println(kataBalik. reverse ());
+    }   
+}
